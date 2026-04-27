e8bb9fdaacd5a021cc17b114a6edd452b08ccca3
Vanshaj Jain <dev.vanshaj@gmail.com>
2026-04-27 14:42:44 +0530

    Adding metadata in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 __author__ = "Vanshaj Jain"
 __version__ = "1.0.0"
+__copyright__ = "2026 Vanshaj Jain"
+__email__ = "dev.vanshaj@gmail.com"
+__description__ = "A Portfolio AI Agent that uses Gemini LLM to analyze financial data and provide insights."
+__maintainer__ = "Vanshaj Jain"
+
+############################################################
 
 from core.config import Config
 from data.database_manager import DatabaseManager
@@ -10,6 +16,7 @@ from tools.sql_generator import SQLTool
 from tools.sector_exposure_calculator import ExposureTool
 from agent.portfolio_ai_agent import PortfolioAgent
 
+############################################################
 
 def main(): 
     # Initialize database connection
