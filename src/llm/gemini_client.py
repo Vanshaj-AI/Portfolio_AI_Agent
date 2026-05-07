95661c06b7a995a732f52544c0fa711a99692327
Vanshaj Jain <dev.vanshaj@gmail.com>
2026-05-07 10:20:36 +0530

    Added Langsmith for Monitoring and replaced deprecated library with new one

--- src/llm/gemini_client.py
+++ src/llm/gemini_client.py
@@ -1,16 +1,36 @@
-import google.generativeai as genai
+from google import genai
+from langsmith import wrappers
 from core.config import Config
+
 
 class GeminiClient:
     def __init__(self):
-        genai.configure(api_key=Config.GEMINI_API_KEY)
-        self.model = genai.GenerativeModel("gemini-3-flash-preview")
+        gemini_client = genai.Client(
+            api_key=Config.GEMINI_API_KEY)
 
-    def generate(self, prompt: str, temperature: float = 0):
-        response = self.model.generate_content(
-            prompt,
-            generation_config=genai.types.GenerationConfig(
-                temperature=temperature
-            )
+        # Wrap with LangSmith tracing
+        self.client = wrappers.wrap_gemini(
+            gemini_client,
+            tracing_extra={
+                "tags": ["gemini"],
+                "metadata": {
+                    "service": "gemini-client"
+                }})
+
+        self.model = "gemini-3-flash-preview"
+
+    def generate(
+        self,
+        prompt: str,
+        temperature: float = 0
+    ):
+
+        response = self.client.models.generate_content(
+            model=self.model,
+            contents=prompt,
+            config={
+                "temperature": temperature
+            }
         )
+
         return response.text.strip()
